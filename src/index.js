17b8c0f4908ee2b5c2c02de3f8c11257de78c768
Kim T <kimturley@gmail.com>
2019-11-12 11:17:43 -0800

    Split into functions

--- src/index.js
+++ src/index.js
@@ -1,71 +1,103 @@
 import './index.scss';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-var texture_bg = 'https://images.designtrends.com/wp-content/uploads/2015/12/17112023/outer-space-Texture.jpg';
-var texture_globe = 'https://i.postimg.cc/nn74Bznd/earth.png';
+function setupRenderer() {
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  return renderer;
+}
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function setupCamera() {
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.set(0, 0, 600);
+  return camera;
+}
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.set(0, 0, 600);
+function setupControls(camera, renderer) {
+  var controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  return controls;
+}
 
-var controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
+function setupScene(camera) {
+  var scene = new THREE.Scene();
+  scene.add(camera);
+  return scene;
+}
 
-var scene = new THREE.Scene();
-scene.add(camera);
+function setupGlobe(scene) {
+  var texture_bg = 'https://images.designtrends.com/wp-content/uploads/2015/12/17112023/outer-space-Texture.jpg';
+  var texture_globe = 'https://i.postimg.cc/nn74Bznd/earth.png';
 
-var loader = new THREE.TextureLoader();
-loader.load(texture_bg, function(texture) {
-  scene.background = texture;  
-});
+  var loader = new THREE.TextureLoader();
+  loader.load(texture_bg, function (texture) {
+    scene.background = texture;
+  });
 
-var pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.position.set(10, 50, 400);
-scene.add(pointLight);
+  var pointLight = new THREE.PointLight(0xFFFFFF);
+  pointLight.position.set(10, 50, 400);
+  scene.add(pointLight);
 
-var globe = new THREE.Group();
-scene.add(globe);
+  var globe = new THREE.Group();
+  scene.add(globe);
 
-var loader2 = new THREE.TextureLoader();
-loader2.load(texture_globe, function (texture) {
-  var sphere = new THREE.SphereGeometry(200, 50, 50);
-  var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
-  var mesh = new THREE.Mesh(sphere, material);
-  globe.add(mesh);
-});
+  var loader2 = new THREE.TextureLoader();
+  loader2.load(texture_globe, function (texture) {
+    var sphere = new THREE.SphereGeometry(200, 50, 50);
+    var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
+    var mesh = new THREE.Mesh(sphere, material);
+    globe.add(mesh);
+  });
 
-document.onkeydown = function(e) {
-  e = e || window.event;
-  e.preventDefault();
-  if (e.keyCode == '38') {
-    globe.rotation.x -= 0.2;
-  }
-  else if (e.keyCode == '40') {
-    globe.rotation.x += 0.2;
-  }
-  else if (e.keyCode == '37') {
-    globe.rotation.y -= 0.2;
-  }
-  else if (e.keyCode == '39') {
-    globe.rotation.y += 0.2;
+  return globe;
+}
+
+function setupKeyboard(globe) {
+  document.onkeydown = function (e) {
+    e = e || window.event;
+    e.preventDefault();
+    if (e.keyCode == '38') {
+      globe.rotation.x -= 0.1;
+    } else if (e.keyCode == '40') {
+      globe.rotation.x += 0.1;
+    } else if (e.keyCode == '37') {
+      globe.rotation.y -= 0.1;
+    } else if (e.keyCode == '39') {
+      globe.rotation.y += 0.1;
+    }
   }
 }
 
-window.addEventListener('resize', function() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+function setupResize(camera, renderer) {
+  window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+}
 
-var animate = function() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);
-};
+function setupAnimate(controls, renderer, scene, camera) {
+  var animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  };
+  animate();
+}
 
-animate();
+function setup() {
+  const renderer = setupRenderer();
+  const camera = setupCamera();
+  const controls = setupControls(camera, renderer);
+  const scene = setupScene(camera);
+  const globe = setupGlobe(scene);
+  setupKeyboard(globe);
+  setupResize(camera, renderer);
+  setupAnimate(controls, renderer, scene, camera);
+}
+
+setup();
