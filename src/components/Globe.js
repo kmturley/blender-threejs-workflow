a6f030533f8f721850aaf3b5920c26e2d99aec9b
Kim T <kimturley@gmail.com>
2019-12-05 15:00:41 -0800

    Use JS classes

---
+++ src/components/Globe.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three';
+
+export class Globe {
+  options = {};
+  constructor(base, options) {
+    console.log('Globe', this);
+    this.options = {...this.options, ...options};
+    this.base = base;
+    this.sky = this.setupSky(base, options.sky);
+    this.sphere = this.setupSphere(base);
+  }
+
+  setupSky(base, sky) {
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([sky, sky, sky, sky, sky, sky]);
+    base.scene.background = texture;
+    return base.scene.background;
+  }
+
+  setupSphere(base) {
+    // Example using basic background
+    // const loader = new THREE.TextureLoader();
+    // loader.load(texture_bg, (texture) => {
+    //   base.scene.background = texture;
+    // });
+  
+    // Example using basic texture
+    // const texture_globe = 'https://i.postimg.cc/nn74Bznd/earth.png';
+    // const globe = new THREE.Group();
+    // base.scene.add(globe);
+    // const loader2 = new THREE.TextureLoader();
+    // loader2.load(texture_globe, (texture) => {
+    //   const sphere = new THREE.SphereGeometry(200, 50, 50);
+    //   const material = new THREE.MeshBasicMaterial({map: texture});
+    //   const mesh = new THREE.Mesh(sphere, material);
+    //   globe.add(mesh);
+    // });
+    // return globe;
+  
+    // Example using flat shading
+    const geometry = new THREE.SphereBufferGeometry(this.options.radius, 12, 9);
+    const material = new THREE.MeshPhongMaterial({
+      flatShading: true,
+      color: 0x0000ff,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    base.scene.add(mesh);
+    return mesh;
+  }
+
+  randomRotation() {
+    return Math.random() * 360;
+  }
+
+  addModel(model, offset = 0) {
+    const stick = new THREE.Object3D();
+    model.position.set(0, this.options.radius + offset, 0);
+    stick.add(model);
+    stick.rotation.set(this.randomRotation(), this.randomRotation(), this.randomRotation());
+    this.base.scene.add(stick);
+  }
+}
---
+++ src/components/Model.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+export class Model {
+  options = {};
+  constructor(base, globe, options) {
+    console.log('Model', this);
+    this.options = {...this.options, ...options};
+    this.loader = new GLTFLoader();
+    if (options.decoder) {
+      const dracoLoader = new DRACOLoader();
+      dracoLoader.setDecoderPath(options.decoder);
+      this.loader.setDRACOLoader(dracoLoader);
+    }
+    this.load(base, globe, options.path, options.scale);
+  }
+
+  load(base, globe, path, scale) {
+    this.loader.load(path, (gltf) => {
+      gltf.scene.scale.set(scale, scale, scale);
+      globe.addModel(gltf.scene);
+      base.addAnimation(gltf.scene, gltf);
+    });
+  }
+}
